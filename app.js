8f78c6b438aee467eefded15e314a54821203186
Kenneth Alvarado <gatotico99@gmail.com>
2023-12-30 08:15:21 -0600

    Added js

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 // Acount Form
 
+const registerButton = document.getElementById("register");
+const loginButton = document.getElementById("login");
+const containerButton = document.getElementById("container");
+
+registerButton.addEventListener("click", () => {
+    container.classList.add("right-panel-active");
+});
+
+loginButton.addEventListener("click", () => {
+    container.classList.remove("right-panel-active");
+});
